d75a76e4cf8a94fee70a29d0fbfcb04b19ecd3d0
Gnanasambandham Balasundaram <gnanamavc2003@gmail.com>
2025-05-17 05:30:14 +0000

    SAVE

--- models/staging/models/staging/customers.sql
+++ models/staging/models/staging/customers.sql
@@ -1,3 +1,11 @@
-select * from DBT_DB.confirmed_layer.stg_customers
+{{ config(materialized='table') }}
+
+WITH tb1 as(
+    select
+    id ,
+    first_name,
+    last_name
+    from {{source('datafeed_shared_schema','stg_customerdata')}})
+select * from tb1
 
 
