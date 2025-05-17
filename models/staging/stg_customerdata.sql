c067d826a43cfb345cf88370bf156bc4ab8b09bb
Gnanasambandham Balasundaram <gnanamavc2003@gmail.com>
2025-05-17 04:49:07 +0000

    update

--- models/staging/stg_customerdata.sql
+++ models/staging/stg_customerdata.sql
@@ -1,9 +1,12 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='view') 
+    }}
 
 WITH tb1  as(
  select
-        id ,
-        first_name,
-        last_name
-     from {{source('datafeed_shared_schema','STG_CUSTOMERS')}})
+        id,
+        order_date,
+        user_id
+        
+     from {{source('datafeed_shared_schema','STG_ORDERS')}})
      select * from tb1
